1db3d57d39cf3374b475b59951da12accb8e9d73
Fabio Miranda <fabiodesenamiranda@hotmail.com>
2022-05-27 19:47:20 -0300

    atualização dos exercicios 3

--- utilizandoJSexterno.js
+++ utilizandoJSexterno.js
@@ -1,2 +1,13 @@
-let name = prompt("Nome da pessoa mais velha")
-console.log(name)
+let oldPersonName = prompt("Nome da pessoa mais velha")
+let olderPersonAge = prompt ("Idade da pessoa mais velha")
+
+let youngPersonName = prompt("Nome da pessoa mais nova")
+let youngPersonAge = prompt ("Idade da pessoa mais nova")
+
+let ageDifference = olderPersonAge - youngPersonAge
+
+alert(
+    "Pessoa mais velha: " + oldPersonName + "\nIdade:" + olderPersonAge + 
+    "\n\nPessoa mais nova: " + youngPersonName + "\nIdade: " + youngPersonAge + 
+    "\n\nDiferença de Idade: " + ageDifference
+)
